a7312900f1453fb7b64ed87395428587a7791ab9
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-05-23 09:11:56 +0200

    Handle kwargs

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import inspect
 
 
-def smartprint(*argv):
+def smartprint(*argv, **kwargs):
     """
     The string representation of the argument is
     concatenated to its value
@@ -9,6 +9,6 @@ def smartprint(*argv):
     try:
         line_of_code = inspect.getframeinfo(inspect.stack()[1][0]).code_context[0]
         message = line_of_code.replace("smartprint", "").replace("sprint", "").strip()[1:-1]
-        print(message, ":", *argv)
+        print(message, ":", *argv, **kwargs)
     except:
-        print(*argv)
+        print(*argv, **kwargs)
