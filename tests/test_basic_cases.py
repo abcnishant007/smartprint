bcefff428abdb4328bbdc83e4fcab7d391f882eb
NISHANT KUMAR <abc.nishant007@gmail.com>
2022-05-27 19:51:17 +0800

    Added first tests

---
+++ tests/test_basic_cases.py
@@ -0,0 +1,43 @@
+import unittest
+import sys
+sys.path.append("/Users/nishant/smartprint")
+from __init__ import smartprint as sprint
+import io
+import numpy as np
+
+
+class SmartPrintTests(unittest.TestCase):
+    def test_numeric_value(self):
+        old_stdout = sys.stdout
+
+        redir_to_var = io.StringIO()
+        sys.stdout = redir_to_var
+
+        # actual call
+        a = 25
+        sprint(a)
+
+        assert redir_to_var.getvalue().strip() == "a : 25"
+
+        sys.stdout = old_stdout
+
+
+    def test_other_objects(self):
+        redir_to_var = io.StringIO()
+        old_stdout = sys.stdout
+        sys.stdout = redir_to_var
+
+        # actual call
+        np.random.seed(0)
+        sprint(np.random.rand())
+
+        assert redir_to_var.getvalue().strip() == "np.random.rand() : 0.5488135039273248"
+
+        sys.stdout = old_stdout
+
+
+
+# if __name__ == "__main__":
+#     SmartPrintTests.test_numeric_value()
+#     SmartPrintTests.test_other_objects()
+#     # SmartPrintTests.test_other_objects()
