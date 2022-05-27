fa19373380edccd499a29fd2c87ce42279443cbe
NISHANT KUMAR <abc.nishant007@gmail.com>
2022-05-27 22:43:55 +0800

    #8 based on user @nickdelgrosso input

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import inspect
 
 
-def smartprint(*argv):
+def smartprint(*argv, **kwargs):
     """
     The string representation of the argument is
     concatenated to its value
@@ -10,4 +10,5 @@ def smartprint(*argv):
     opening_bracket_location = line_of_code.find("(") + 1
     closing_bracket_location = line_of_code.rfind(")")
 
-    print(line_of_code[opening_bracket_location:closing_bracket_location], ":", *argv)
+    print(line_of_code[opening_bracket_location:closing_bracket_location], ":", *argv, **kwargs)
+
