1138237790c5f7b2e369f9d09b1d211902ab043e
NISHANT KUMAR <abc.nishant007@gmail.com>
2022-07-15 00:04:13 +0800

    Create __init__.py

---
+++ src/smartprint/__init__.py
@@ -0,0 +1,38 @@
+import inspect
+from pprint import pprint
+
+
+def smartprint(*argv, **kwargs):
+    """
+    The string representation of the argument is
+    concatenated to its value
+    """
+
+    line_of_code = inspect.getframeinfo(inspect.stack()[1][0]).code_context[0].strip()
+    opening_bracket_location = line_of_code.find("(") + 1
+    closing_bracket_location = line_of_code.rfind(")")
+    extracted_code = line_of_code[opening_bracket_location:closing_bracket_location]
+
+    # additional test to make sure extra brackets do not wreck our code
+    extracted_code = extracted_code.replace("sprint", "").replace("smartprint", "").replace("prints","")
+
+    if len(argv) == 1 and len(kwargs) == 0:
+        if isinstance(argv[0], list):
+            helper_smartprint_list(argv[0], extracted_code)
+        elif isinstance(argv[0], dict):
+            helper_smartprint_dict(argv[0], extracted_code)
+        else:
+            print(extracted_code, ":", *argv, **kwargs)
+    else:
+        print(extracted_code, ":", *argv, **kwargs)
+
+
+def helper_smartprint_list(l, extracted_code):
+    print("List:", extracted_code)
+    pprint(l)
+
+
+def helper_smartprint_dict(d, extracted_code):
+    print("Dict:", extracted_code)
+    print("Key: Value")
+    pprint(d)
