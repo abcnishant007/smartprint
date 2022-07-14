9eecc7920517ed8c86926c1b52bc781e950206f8
NISHANT KUMAR <abc.nishant007@gmail.com>
2022-07-15 00:40:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="smartprint",
-    version="0.1.3",
+    version="0.1.5",
     author="abcnishant007",
     author_email="abc.nishant007@gmail.com",
     description="Include the variable name in print statements",
